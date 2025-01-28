285bc9110e1c2a15a27877a1509b397ea636a227
root <benbazirake9@gmail.com>
2025-01-28 01:55:40 -0800

    add the file to the epo

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+def create_database():
+    try:
+        connection= mysql.connector.connect(
+            Host= "local@host"
+            User="bazirake"
+            Password="20250228"
+        )
+        if connection.is_connected():
+            cusor=connection.cusor()
+            cusor.execute=("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+    except KeyError as e :
+        print(F"{e}")
+    finally:
+        if connection.is_connected():
+            cusor.close()
+            connection.close()
+if __name__ == "__main__":
+  create_database()
+
+    
