ea5dc4c28bf0d4b29f1a7accdc5bf921c6c93340
root <benbazirake9@gmail.com>
2025-01-28 03:02:45 -0800

    add the change to the  file

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,5 @@
-USE alx_book_store;
-SELECET book_id, title,author_id, price, pulication_date FROM books;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'Books';
+
